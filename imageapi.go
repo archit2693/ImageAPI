9e2f3fa9960e4eca04e6817a5fbb4e94d6c00105
Archit joshi <architjoshi@Archits-MacBook-Pro.local>
2018-05-27 21:19:21 +0530

    Add image not found checking

--- imageapi.go
+++ imageapi.go
@@ -3,15 +3,35 @@ package main
 import(
 	"net/http"
 	"log"
+	"os"
 )
+
+func DoesImageExist(image_name string) bool {
+     _, error := os.Stat(image_name)
+     if !os.IsNotExist(error) {
+       return  true
+     } else {
+       return false
+     }
+}
 
 func ImageHandler(writer http.ResponseWriter, request *http.Request){
     image_name, name_status := request.URL.Query()["image_name"]
     image_width, width_status := request.URL.Query()["width"]
     image_height, height_status := request.URL.Query()["height"]
     if !name_status || !width_status || !height_status || len(image_name) < 1 || len(image_width) < 1 || len(image_height) < 1 {
-       ReportError(writer, "Sorry, Invaild Parameters.", 500)
+       ReportError(writer, "Sorry, Invalid Parameters.", 500)
+       return
     }
+
+   // requested_image_name = image_name[0]
+    image_name[0] = "images/"+image_name[0]
+    if !DoesImageExist(image_name[0]) {
+     	ReportError(writer, "Sorry, the image you requested is not found.", 404)
+	return
+    }
+    
+
 }
 
 func main(){
--- imageapi_test.go
+++ imageapi_test.go
@@ -5,6 +5,7 @@ import(
 	"net/http"
 	"net/http/httptest"
 	"strings"
+	"fmt"
 )
 
 func init(){
@@ -20,13 +21,26 @@ func TestCorrectResponse(t *testing.T){
      }
 }
 
-func TestParamters(t *testing.T){
+func TestParamtersPresence(t *testing.T){
      request := httptest.NewRequest(http.MethodGet, "/api/resize", nil)
      response_recorder := httptest.NewRecorder()
      http.DefaultServeMux.ServeHTTP(response_recorder, request)
      expected := "Sorry, Invalid Parameters."
+     
      actual := response_recorder.Body.String()
-     if strings.Contains(actual, expected) {
+      fmt.Println(actual)
+     if !strings.Contains(actual, expected){
      	t.Errorf("handler returned unexpected body: got %v want %v", actual, expected)
 	}
+}
+
+func TestFileAbsence(t *testing.T) {
+     request := httptest.NewRequest(http.MethodGet, "/api/resize?image_name=test.jpg&width=600&height=500", nil)
+     response_recorder := httptest.NewRecorder()
+     http.DefaultServeMux.ServeHTTP(response_recorder, request)
+     expected := "Sorry, the image you requested is not found."
+     actual := response_recorder.Body.String()
+     if !strings.Contains(actual, expected){
+     	t.Errorf("handler returned unexpected body: got %v want %v ", actual, expected)
+     }
 }
